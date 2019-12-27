7a0dcd80c50b22684f3529e0af67435f78e5b088
pengcheng <dupengcheng@dupengchengdeMacBook-Pro.local>
2019-12-27 15:08:39 +0800

    1、优化8.0系统打开app闪退的问题

--- app/src/main/java/com/shmedo/mcloudapp/util/UiUtils.java
+++ app/src/main/java/com/shmedo/mcloudapp/util/UiUtils.java
@@ -1,13 +1,11 @@
 package com.shmedo.mcloudapp.util;
 
-import androidx.core.content.FileProvider;
-
 /**
  * 项目名：  mCloudapp
  * 包名：    com.shmedo.mcloudapp.util
  * 创建者:   dpc
- * 创建时间:  2019-12-24
+ * 创建时间:  2019-12-27
  * 描述：    TODO
  */
-public class GenericFileProvider extends FileProvider {
+public class UiUtils {
 }
