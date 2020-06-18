933a3761b53ad5d5f21dc26262893ec8f5710334
gonghe <gongheoffice@163.com>
2020-06-18 09:28:23 +0800

    1.Bug 修改

--- app/src/main/java/com/shmedo/mcloudapp/maps/ui/activity/MapActivity.java
+++ app/src/main/java/com/shmedo/mcloudapp/maps/ui/activity/MapActivity.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 
 import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
@@ -103,8 +105,21 @@ public class MapActivity extends CheckMapNeedPermissionsActivity implements AMap
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initStates();
         initView(savedInstanceState);
         setListener();
+    }
+
+    /**
+     * 沉浸式状态栏
+     */
+    private void initStates() {
+        if (Build.VERSION.SDK_INT > 19 && getApplicationContext().getApplicationInfo().targetSdkVersion > 19) {
+            //透明状态栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //透明导航栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
     }
 
     @Override
