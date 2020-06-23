d408b96096716f1691c0ce955b5500fe0c9fe6c0
gonghe <gongheoffice@163.com>
2020-06-23 11:27:11 +0800

    1.截屏Bug 修改

--- app/src/main/java/com/shmedo/mcloudapp/maps/util/ScreenShotAction.java
+++ app/src/main/java/com/shmedo/mcloudapp/maps/util/ScreenShotAction.java
@@ -3,9 +3,12 @@ package com.shmedo.mcloudapp.maps.util;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
 
@@ -13,6 +16,7 @@ import com.afollestad.materialdialogs.MaterialDialog;
 import com.hjq.toast.ToastUtils;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -25,8 +29,6 @@ import timber.log.Timber;
  */
 
 public class ScreenShotAction extends AsyncTask<Void, Integer, File> {
-    public static final String SYSTEM_SCRENN_PREFIX = "sys#screen&prefix.";
-
     @SuppressLint("StaticFieldLeak")
     private Activity activity = null;
 
@@ -57,21 +59,15 @@ public class ScreenShotAction extends AsyncTask<Void, Integer, File> {
         bitmap = view.getDrawingCache();
         showLoadingDialog(activity, "处理中");
 
-        view.setDrawingCacheEnabled(false);
-        view.destroyDrawingCache();
+//        view.setDrawingCacheEnabled(false);
+//        view.destroyDrawingCache();
     }
 
     @Override
     protected File doInBackground(Void... params) {
         String filename = null;
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
-        if (hasExternalStorageState()) {
-            filename = Environment.getExternalStorageDirectory().getPath() + File.separator + activity.getPackageName()
-                    + File.separator + "ScreenShot" + File.separator + sdf.format(new Date()) + ".png";
-        } else {
-            filename = activity.getApplicationContext().getFilesDir().getAbsolutePath()
-                    + File.separator + "ScreenShot" + File.separator + sdf.format(new Date()) + ".png";
-        }
+        filename = activity.getExternalFilesDir(null).getAbsolutePath() + File.separator + "ScreenShot" + File.separator + sdf.format(new Date()) + ".png";
 
         Timber.d("filename:" + filename);
         File file = new File(filename);
@@ -82,53 +78,57 @@ public class ScreenShotAction extends AsyncTask<Void, Integer, File> {
     }
 
     @Override
-    protected void onPostExecute(File result) {
+    protected void onPostExecute(File file) {
         dismissLoadingDialog();
-        ToastUtils.show("截图完成");
-//        DialogMaker.dismissProgressDialog();
-//        if (result != null)
-//        {
-//            SessionHelper.startP2PSession(activity, account);
-//            IMMessage message = MessageBuilder.createImageMessage(account, SessionTypeEnum.P2P, result, SYSTEM_SCRENN_PREFIX + result.getPath());
-//            NIMClient.getService(MsgService.class).sendMessage(message, false);
-//        }
+        ToastUtils.show("截图已保存到相册");
+        if (file != null && file.exists()) {
+            boolean result = file.delete();
+        }
     }
 
     /**
      * 保存Bitmap图片为本地文件
      */
 
-    public static boolean saveFile(Bitmap bitmap, File file) {
+    private boolean saveFile(Bitmap bitmap, File fileImage) {
         boolean isOk = true;
-        FileOutputStream fileOutputStream = null;
 
         try {
-            File dir = file.getParentFile();
+            File dir = fileImage.getParentFile();
             if (dir != null) {
                 if (!dir.exists()) {
                     dir.mkdirs();
                 }
             }
-
-            if (!file.exists())
-            {
-                file.createNewFile();
+            if (!fileImage.exists()) {
+                fileImage.createNewFile();
             }
 
-            fileOutputStream = new FileOutputStream(file);
+            FileOutputStream fileOutputStream = new FileOutputStream(fileImage);
             bitmap.compress(Bitmap.CompressFormat.PNG, 90, fileOutputStream);
             fileOutputStream.flush();
             fileOutputStream.close();
-
         } catch (IOException e) {
             isOk = false;
             e.printStackTrace();
         }
 
+        // 其次把文件插入到系统图库
+        try {
+            MediaStore.Images.Media.insertImage(activity.getContentResolver(), fileImage.getAbsolutePath(), fileImage.getName(), null);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        // 最后通知图库更新
+        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        Uri uri = Uri.fromFile(fileImage);
+        intent.setData(uri);
+        activity.sendBroadcast(intent);
+
         return isOk;
     }
 
-    private static boolean hasExternalStorageState() {
+    private boolean hasExternalStorageState() {
         String status = Environment.getExternalStorageState();
         if (status.equals(Environment.MEDIA_MOUNTED)) {
             return true;
