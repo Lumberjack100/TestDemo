8dec8f159135c4f12815bfaac02dcc8f06164a77
gonghe <gongheoffice@163.com>
2020-08-19 15:38:57 +0800

    1.修改项目介绍信息页面

--- app/src/main/java/com/shmedo/mcloudapp/projects/ui/activity/ProjectIntroductionActivity.java
+++ app/src/main/java/com/shmedo/mcloudapp/projects/ui/activity/ProjectIntroductionActivity.java
@@ -2,14 +2,23 @@ package com.shmedo.mcloudapp.projects.ui.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.ColorUtils;
 
+import com.bumptech.glide.Glide;
+import com.gyf.immersionbar.ImmersionBar;
 import com.shmedo.mcloudapp.R;
 import com.shmedo.mcloudapp.common.ui.activity.BaseActivity;
 import com.youth.banner.Banner;
+import com.youth.banner.adapter.BannerImageAdapter;
+import com.youth.banner.config.IndicatorConfig;
+import com.youth.banner.holder.BannerImageHolder;
+import com.youth.banner.indicator.CircleIndicator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +27,7 @@ import butterknife.BindView;
 
 public class ProjectIntroductionActivity extends BaseActivity {
     @BindView(R.id.toolbar)
-    Toolbar toolbar;
+    Toolbar mToolbar;
 
     @BindView(R.id.toolbar_title)
     TextView mToolbarTitle;
@@ -56,26 +65,47 @@ public class ProjectIntroductionActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setToolBar(R.id.toolbar);
-        mToolbarTitle.setText("关于");
+        mToolbarTitle.setText("项目简介");
         setBanner();
         initData();
     }
 
-    private void setBanner() {
+    /**
+     * 初始化沉浸式
+     * Init immersion bar.
+     */
+    @Override
+    protected void initImmersionBar() {
+        mToolbar.setBackgroundColor(ColorUtils.blendARGB(Color.TRANSPARENT
+                , ContextCompat.getColor(this, R.color.white), 0));
+        ImmersionBar.with(this)
+                .titleBar(mToolbar)
+                .statusBarColor(R.color.transparent, 0)
+                .statusBarDarkFont(false)
+                .navigationBarDarkIcon(true)
+                .navigationBarColor(R.color.white)
+                .init();
+    }
 
-//        banner.setAdapter(new BannerImageAdapter<String>(imgUrlList) {
-//            @Override
-//            public void onBindView(BannerImageHolder holder, String imageUrl, int position, int size) {
-//                //图片加载自己实现
-//                Glide.with(holder.itemView)
-//                        .load(imageUrl)
-//                        .thumbnail(Glide.with(holder.itemView).load(R.drawable.loading))
-//                        .into(holder.imageView);
-//            }
-//        });
-//        banner.addBannerLifecycleObserver(this);
-//        banner.setIndicator(new CircleIndicator(this));
-//        banner.setIndicatorGravity(IndicatorConfig.Direction.CENTER);
+    private void setBanner() {
+        imgUrlList.add("https://img.zcool.cn/community/013de756fb63036ac7257948747896.jpg");
+        imgUrlList.add("https://img.zcool.cn/community/01639a56fb62ff6ac725794891960d.jpg");
+        imgUrlList.add("https://img.zcool.cn/community/01270156fb62fd6ac72579485aa893.jpg");
+        imgUrlList.add("https://img.zcool.cn/community/01233056fb62fe32f875a9447400e1.jpg");
+
+        banner.setAdapter(new BannerImageAdapter<String>(imgUrlList) {
+            @Override
+            public void onBindView(BannerImageHolder holder, String imageUrl, int position, int size) {
+                //图片加载自己实现
+                Glide.with(holder.itemView)
+                        .load(imageUrl)
+                        .thumbnail(Glide.with(holder.itemView).load(R.drawable.loading))
+                        .into(holder.imageView);
+            }
+        });
+        banner.addBannerLifecycleObserver(this);
+        banner.setIndicator(new CircleIndicator(this));
+        banner.setIndicatorGravity(IndicatorConfig.Direction.CENTER);
 
     }
 
