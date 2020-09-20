b76d78b90c2a6dd399581686a35582951cbc641d
gonghe <gongheoffice@163.com>
2020-09-20 22:27:18 +0800

    1.设备蓝牙指令调试输出页面修改

--- app/src/main/java/com/shmedo/mcloudapp/deviceconfig/ui/activity/CustomCommandLogPrintActivity.java
+++ app/src/main/java/com/shmedo/mcloudapp/deviceconfig/ui/activity/CustomCommandLogPrintActivity.java
@@ -7,34 +7,51 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.hjq.toast.ToastUtils;
 import com.shmedo.core.AppContants;
+import com.shmedo.core.util.LogFileUtil;
 import com.shmedo.mcloudapp.R;
 import com.shmedo.mcloudapp.common.ui.activity.BaseActivity;
 import com.shmedo.mcloudapp.deviceconfig.ui.fragment.BleCollectorSettingFragment;
 import com.shmedo.mcloudapp.deviceconfig.ui.fragment.BleCustomCommandLogPrintFragment;
 import com.shmedo.mcloudapp.deviceconfig.ui.fragment.NetCollectorSettingFragment;
+import com.shmedo.mcloudapp.util.FileProviderUtils;
+
+import java.io.File;
 
 import butterknife.BindView;
+import butterknife.OnClick;
+import gdut.bsx.share2.Share2;
+import gdut.bsx.share2.ShareContentType;
 
+/**
+ *
+ */
 public class CustomCommandLogPrintActivity extends BaseActivity {
     @BindView(R.id.tv_title)
     TextView mToolbarTitle;
+
+    @BindView(R.id.right_icon)
+    ImageView mIvRightIcon;
 
     private int connectWay = AppContants.CommunicationWay.NET_CONNECT;
 
     private Fragment fragment;
 
     public static void startActivity(Context context) {
-        Intent intent = new Intent(context, IOTCollectorSettingActivity.class);
+        Intent intent = new Intent(context, CustomCommandLogPrintActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
         context.startActivity(intent);
     }
 
     public static void startActivity(Context context, int connectWay) {
-        Intent intent = new Intent(context, IOTCollectorSettingActivity.class);
+        Intent intent = new Intent(context, CustomCommandLogPrintActivity.class);
         intent.putExtra(AppContants.Extras.COMMUNICATION_WAY, connectWay);
         intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
         context.startActivity(intent);
@@ -51,6 +68,8 @@ public class CustomCommandLogPrintActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setToolBar(R.id.toolbar);
         mToolbarTitle.setText("指令日志输出");
+        mIvRightIcon.setVisibility(View.VISIBLE);
+        mIvRightIcon.setImageResource(R.drawable.icon_share_command_log);
         parseIntent();
         initFragment();
     }
@@ -80,5 +99,31 @@ public class CustomCommandLogPrintActivity extends BaseActivity {
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.container, fragment);
         transaction.commitAllowingStateLoss();
+    }
+
+    @OnClick({R.id.right_icon})
+    public void onClick(View v) {
+        if (v.getId() == R.id.right_icon) {
+            shareFile();
+        }
+    }
+
+    private void shareFile() {
+        String path = LogFileUtil.getLogPath();
+        File file = new File(path);
+        if (!file.exists()) {
+            ToastUtils.show("日志文件不存在");
+            return;
+        }
+
+        Uri contentUri = FileProviderUtils.uriFromFile(this, file);
+
+        new Share2.Builder(this)
+                .setContentType(ShareContentType.FILE)
+                .setShareFileUri(contentUri)
+                .setTitle("分享文件")
+                .setOnActivityResult(300)
+                .build()
+                .shareBySystem();
     }
 }
--- app/src/main/java/com/shmedo/mcloudapp/deviceconfig/ui/fragment/BleCustomCommandLogPrintFragment.java
+++ app/src/main/java/com/shmedo/mcloudapp/deviceconfig/ui/fragment/BleCustomCommandLogPrintFragment.java
@@ -3,24 +3,75 @@ package com.shmedo.mcloudapp.deviceconfig.ui.fragment;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.hjq.toast.ToastUtils;
+import com.kyleduo.switchbutton.SwitchButton;
+import com.lxj.xpopup.XPopup;
+import com.lxj.xpopup.interfaces.OnSelectListener;
+import com.shmedo.configlibrary.ble.cmd.CommandManager;
+import com.shmedo.configlibrary.ble.cmd.entity.LogOutputEntity;
+import com.shmedo.configlibrary.ble.cmd.entity.WorkModeEntity;
+import com.shmedo.configlibrary.ble.enums.CommandType;
+import com.shmedo.configlibrary.ble.enums.LogOutputStatus;
+import com.shmedo.configlibrary.ble.enums.WorkModel;
+import com.shmedo.core.MCloudApp;
 import com.shmedo.core.event.BluetoothConnectStateEvent;
 import com.shmedo.core.event.CmdResponseMessage;
 import com.shmedo.core.event.MessageEvent;
 import com.shmedo.mcloudapp.R;
+import com.shmedo.mcloudapp.common.view.ClearEditText;
+import com.zhy.adapter.recyclerview.CommonAdapter;
+import com.zhy.adapter.recyclerview.base.CommonViewHolder;
 
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+import me.pqpo.librarylog4a.Log4a;
+import timber.log.Timber;
 
 /**
  * 自定义蓝牙指令交互输出并保存日志文件
  */
 public class BleCustomCommandLogPrintFragment extends BaseBleConnectFragment {
+    private static final String TAG = "BleCustomCommandLogPrintFragment";
+
+    @BindView(R.id.tv_debug_mode)
+    TextView mTvDebugMode;
+
+    @BindView(R.id.logPrintEnableSBtn)
+    SwitchButton logSwitchButton;
+
+    @BindView(R.id.ce_send_code)
+    ClearEditText ceSendCode;
+
+    @BindView(R.id.recyclerView_log)
+    RecyclerView mRecyclerView;
+
+    @BindView(R.id.fab_start_pause)
+    FloatingActionButton fabStartPause;
+
+    private CommonAdapter adapter;
+
+    private List<String> logDataList = new ArrayList<>();
+
+    private boolean isPause = false;
+
+    private String snNumber;
+
+    private int debugModePos = 0;
 
     @Override
     protected int getLayoutId() {
@@ -30,20 +81,190 @@ public class BleCustomCommandLogPrintFragment extends BaseBleConnectFragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        setView();
+        setSwitchViewListener();
+        initAdapter();
+    }
+
+    private void setView() {
+        snNumber = MCloudApp.getCurDeviceToken();
+        String content = String.format("====开始调试设备：%s", snNumber);
+        Log4a.i(TAG, content);
+
+        mTvDebugMode.setText("关闭");
+    }
+
+    /**
+     * switch按钮事件
+     */
+    private void setSwitchViewListener() {
+        //日志输出开关
+        logSwitchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, final boolean isChecked) {
+                if (!MCloudApp.isIsBluetoothDeviceConnected()) {
+                    ToastUtils.show(getString(R.string.ble_config_disconnect_warn));
+                    logSwitchButton.setCheckedImmediatelyNoEvent(!isChecked);
+                    return;
+                }
+
+                if (isChecked) {
+                    switchLogOutputMode(true);
+//                    setSwitchViewState(true, logSwitchButton, "已开启");
+                    ToastUtils.show("开始日志输出");
+                    isPause = false;
+                    fabStartPause.setImageResource(R.drawable.icon_command_log_print_pause);
+                } else {
+                    switchLogOutputMode(false);
+//                    setSwitchViewState(false, logSwitchButton, "已关闭");
+                    ToastUtils.show("关闭日志输出");
+                    isPause = true;
+                    fabStartPause.setImageResource(R.drawable.icon_command_log_print_play);
+                }
+            }
+        });
+    }
+
+    private void initAdapter() {
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(mActivity));
+        adapter = new CommonAdapter<String>(mActivity, R.layout.item_log_print, logDataList) {
+            @Override
+            protected void convert(CommonViewHolder holder, String string, int position) {
+                holder.setText(R.id.tv_log, string);
+            }
+        };
+        mRecyclerView.setAdapter(adapter);
     }
 
 
+    @OnClick({R.id.debugModeLayout, R.id.fab_clear_log, R.id.fab_start_pause, R.id.btn_send})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.debugModeLayout:
+                showDebugModeDialog();
+                break;
+
+            case R.id.fab_clear_log:
+                logDataList.clear();
+                adapter.notifyDataSetChanged();
+                break;
+
+            case R.id.fab_start_pause:
+                if (isPause) {
+                    isPause = false;
+                    ToastUtils.show("日志已开始输出");
+                    fabStartPause.setImageDrawable(getResources().getDrawable(R.drawable.icon_command_log_print_pause));
+                } else {
+                    ToastUtils.show("日志已暂停输出");
+                    isPause = true;
+                    fabStartPause.setImageDrawable(getResources().getDrawable(R.drawable.icon_command_log_print_play));
+                }
+                break;
+
+            case R.id.btn_send:
+                if (!MCloudApp.isIsBluetoothDeviceConnected()) {
+                    ToastUtils.show(getString(R.string.ble_config_disconnect_warn));
+                    return;
+                }
+                //发送指令
+                String sendCode = ceSendCode.getText().toString().trim();
+                String result = sendCode + "\r\n";
+                if (!sendCode.startsWith("##")) {
+                    ToastUtils.show("指令格式不正确，请重新输入");
+                    return;
+                }
+                sendCommonCommandImmediately(result);
+                logDataList.add(sendCode);
+                adapter.notifyDataSetChanged();
+                break;
+
+        }
+    }
+
+    private void showDebugModeDialog() {
+        XPopup.setPrimaryColor(getResources().getColor(R.color.blue_52B4F8));
+        new XPopup.Builder(mActivity)
+                .isDestroyOnDismiss(true) //对于只使用一次的弹窗，推荐设置这个
+                .asBottomList("", new String[]{"关闭", "DEBUG", "INFO"},
+                        null, debugModePos, true,
+                        new OnSelectListener() {
+                            @Override
+                            public void onSelect(int position, String text) {
+                                debugModePos = position;
+                                mTvDebugMode.setText(text);
+                                WorkModel workModel;
+                                switch (text) {
+                                    case "关闭":
+                                        workModel = WorkModel.WORK;
+                                        break;
+
+                                    case "DEBUG":
+                                        workModel = WorkModel.DEBUG;
+                                        break;
+
+                                    case "INFO":
+                                        workModel = WorkModel.INFO;
+                                        break;
+
+                                    default:
+                                        workModel = WorkModel.WORK;
+                                        break;
+                                }
+                                WorkModeEntity workModeEntity = new WorkModeEntity(workModel.toInt());
+                                String command = CommandManager.getInstance().getCommand(CommandType.WORK_MODE, workModeEntity);
+                                sendCommonCommandImmediately(command);
+                                Timber.d("设置调试模式指令==%s", command);
+                            }
+                        }, 0, R.layout.custom_xpopup_adapter_text_match)
+                .show();
+    }
+
+    private void switchLogOutputMode(boolean isOpen) {
+        LogOutputEntity logOutputEntity = new LogOutputEntity(isOpen ? LogOutputStatus.OPEN.toInt() : LogOutputStatus.CLOSE.toInt());
+        String cmd = CommandManager.getInstance().getCommand(CommandType.LOG_OUTPUT_STATUS, logOutputEntity);
+        sendCommonCommandImmediately(cmd);
+    }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onMessageEvent(MessageEvent messageEvent) {
         super.onMessageEvent(messageEvent);
 
-//        if (messageEvent instanceof CmdResponseMessage) {
-//            setResultData((CmdResponseMessage) messageEvent);
-//        } else if (messageEvent instanceof BluetoothConnectStateEvent) {
+        if (messageEvent instanceof CmdResponseMessage) {
+            setResultData((CmdResponseMessage) messageEvent);
+        }
+//        else if (messageEvent instanceof BluetoothConnectStateEvent) {
 //            boolean isConnected = ((BluetoothConnectStateEvent) messageEvent).isConnected;
 //            updateViewStateByConnectState(isConnected);
 //        }
+    }
+
+    private void setResultData(CmdResponseMessage responseMessage) {
+        String cmdStr = responseMessage.getResult();
+        String content = cmdStr.replace("\r\n", "");
+        Timber.i("====日志内容%s", content);
+        Log4a.i(TAG, content);
+        Log4a.flush();
+
+        if (isPause) { //
+            Timber.i("=====暂停了");
+
+        } else {
+            Timber.i("=====开始了");
+            logDataList.add(content);
+            adapter.notifyDataSetChanged();
+            mRecyclerView.scrollToPosition(adapter.getItemCount() - 1);
+        }
+    }
+
+
+    @Override
+    public boolean onBackPressed() {
+        String content = String.format("====结束调试设备：%s\r\n", snNumber);
+        Log4a.i(TAG, content);
+        Log4a.flush();
+        mActivity.finish();
+
+        return true;
     }
 
 }
