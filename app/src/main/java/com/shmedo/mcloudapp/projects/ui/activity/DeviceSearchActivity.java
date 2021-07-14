21f8aab5595f47eda68f121d96e687a549ffa25d
medo_gh <gongheoffice@163.com>
2021-07-14 19:04:28 +0800

    1.Bug 修复

--- app/src/main/java/com/shmedo/mcloudapp/projects/ui/activity/DeviceSearchActivity.java
+++ app/src/main/java/com/shmedo/mcloudapp/projects/ui/activity/DeviceSearchActivity.java
@@ -145,13 +145,12 @@ public class DeviceSearchActivity extends BaseActivity {
                 if (deviceInfo.getDeviceTypeName().contains("DAS")) {
                     deviceType = AppContants.DeviceType.DAS;
                 } else if (deviceInfo.getDeviceTypeName().contains("ADME")) {
-                    ToastUtils.show("此设备暂不支持网络配置");
-                    return;
+                    deviceType = AppContants.DeviceType.ADME;
                 } else if (deviceInfo.getDeviceTypeName().contains("M20")) {
                     deviceType = AppContants.DeviceType.M20;
                 } else if (deviceInfo.getDeviceTypeName().contains("E40") || deviceInfo.getDeviceTypeName().contains("E60")) {
                     deviceType = AppContants.DeviceType.E40;
-                } else if (deviceInfo.getDeviceTypeName().contains("VMS")||deviceInfo.getDeviceTypeName().contains("GW300")) {
+                } else if (deviceInfo.getDeviceTypeName().contains("VMS") || deviceInfo.getDeviceTypeName().contains("GW300")) {
                     deviceType = AppContants.DeviceType.VMS;
                 }
                 if (deviceType == AppContants.DeviceType.UnKnown) {
