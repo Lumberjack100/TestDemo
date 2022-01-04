0f2d0de42ed5bffa8afd718a3e27c503da1e4131
Lumberjack <gongheoffice@163.com>
2022-01-04 09:57:49 +0800

    优化问题

--- app/src/main/java/com/shmedo/mcloudapp/deviceconfig/model/DeviceApiKey.java
+++ app/src/main/java/com/shmedo/mcloudapp/deviceconfig/model/DeviceApiKey.java
@@ -1,31 +1,15 @@
 package com.shmedo.mcloudapp.deviceconfig.model;
 
-import org.greenrobot.greendao.annotation.Entity;
-import org.greenrobot.greendao.annotation.Generated;
-import org.greenrobot.greendao.annotation.Unique;
-
 /**
  * 创建者:   gonghe <br/>
  * 创建时间:  11/25/20 <br/>
  * 描述：     设备 ApiKey 实体
  */
-@Entity
 public class DeviceApiKey {
-    @Unique
     private Integer deviceID;       //设备ID
     private String deviceToken;     //设备token
     private String apiKey;
 
-    @Generated(hash = 257759885)
-    public DeviceApiKey(Integer deviceID, String deviceToken, String apiKey) {
-        this.deviceID = deviceID;
-        this.deviceToken = deviceToken;
-        this.apiKey = apiKey;
-    }
-
-    @Generated(hash = 164323198)
-    public DeviceApiKey() {
-    }
 
     public Integer getDeviceID() {
         return deviceID;
