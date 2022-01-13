074934ba30e6719c5ecf2731b1dc67476fa23aaa
Lumberjack <gongheoffice@163.com>
2022-01-13 14:45:36 +0800

    1.添加 Kotlin 处理，将 Java 类转换成 Kotlin 类

--- core/src/main/java/com/shmedo/core/model/UserPermissionInfo.kt
+++ core/src/main/java/com/shmedo/core/model/UserPermissionInfo.kt
@@ -1,65 +1,26 @@
-package com.shmedo.core.model;
+package com.shmedo.core.model
 
-import android.text.TextUtils;
+import android.text.TextUtils
 
 /**
- * 创建者:   gonghe <br/>
- * 创建时间:  2022/1/11 <br/>
+ * 创建者:   gonghe <br></br>
+ * 创建时间:  2022/1/11 <br></br>
  * 描述：     查询用户在某公司某服务中的所有权限
  */
-public class UserPermissionInfo {
-    private int id;//权限id
-    private String name;//权限名
-    private String permissionToken;//权限token
-    private String serviceName;//权限所属服务
-    private String permissionDesc;//权限描述
-    private String exValues;//权限拓展
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return TextUtils.isEmpty(name) ? "" : name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPermissionToken() {
-        return TextUtils.isEmpty(permissionToken) ? "" : permissionToken;
-    }
-
-    public void setPermissionToken(String permissionToken) {
-        this.permissionToken = permissionToken;
-    }
-
-    public String getServiceName() {
-        return TextUtils.isEmpty(serviceName) ? "" : serviceName;
-    }
-
-    public void setServiceName(String serviceName) {
-        this.serviceName = serviceName;
-    }
-
-    public String getPermissionDesc() {
-        return permissionDesc;
-    }
-
-    public void setPermissionDesc(String permissionDesc) {
-        this.permissionDesc = permissionDesc;
-    }
-
-    public String getExValues() {
-        return exValues;
-    }
-
-    public void setExValues(String exValues) {
-        this.exValues = exValues;
-    }
+ class UserPermissionInfo {
+    var id //权限id
+            = 0
+    var name //权限名
+            : String? = null
+        get() = if (TextUtils.isEmpty(field)) "" else field
+    var permissionToken //权限token
+            : String? = null
+        get() = if (TextUtils.isEmpty(field)) "" else field
+    var serviceName //权限所属服务
+            : String? = null
+        get() = if (TextUtils.isEmpty(field)) "" else field
+    var permissionDesc //权限描述
+            : String? = null
+    var exValues //权限拓展
+            : String? = null
 }
