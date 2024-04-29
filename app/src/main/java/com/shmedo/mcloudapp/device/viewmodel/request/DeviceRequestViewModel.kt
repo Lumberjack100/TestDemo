192ab2815cfdc58590a763da5d889961fcd47b69
Lumberjack <gongheoffice@163.com>
2024-04-29 14:06:03 +0800

    fea_kotlin: 1.注释设备列表查询接口，设备 SN 排序参数

--- app/src/main/java/com/shmedo/mcloudapp/device/viewmodel/request/DeviceRequestViewModel.kt
+++ app/src/main/java/com/shmedo/mcloudapp/device/viewmodel/request/DeviceRequestViewModel.kt
@@ -167,7 +167,7 @@ class DeviceRequestViewModel(private val loggerRepositoryImp: LoggerRepositoryIm
                 )//产品ID,null则不指定产品
                 jsonObjectRequest.put("tokenAndVersion", false)//sn号和版本号之间得关系
                 jsonObjectRequest.put("deviceStatus", "启用")//ull选择全部，启用选择启用设备，禁用用选择未启用设备
-                jsonObjectRequest.put("sortSNAsc", true)//ture按SN正序，false按Sn逆序
+//                jsonObjectRequest.put("sortSNAsc", true)//ture按SN正序，false按Sn逆序
                 jsonObjectRequest.put("onlineStatus", onlineStatus)//在线状态
                 if (isHasListSuperInfoPermission)
                     jsonObjectRequest.put("filterNoPermissionDevice", true)//过滤用户无权限设备
