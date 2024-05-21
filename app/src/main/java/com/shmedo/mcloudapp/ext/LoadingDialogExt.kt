9b5bbc4320933707e6f6109f9bcddcf06a6bd964
Lumberjack <gongheoffice@163.com>
2024-05-21 10:00:10 +0800

    fea_v5.0: 1.优化进度对话框，修复 Can not perform this action after onSaveInstanceState 问题

--- app/src/main/java/com/shmedo/mcloudapp/ext/LoadingDialogExt.kt
+++ app/src/main/java/com/shmedo/mcloudapp/ext/LoadingDialogExt.kt
@@ -1,183 +1,38 @@
 package com.shmedo.mcloudapp.ext
 
-import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
-import com.kongzue.dialogx.dialogs.WaitDialog
-import com.kongzue.dialogx.interfaces.OnBackPressedListener
+import androidx.fragment.app.FragmentActivity
 import com.shmedo.mcloudapp.common.fragment.LoadingDialogFragment
 
-/******************* loading框 版本1 start *************************/
-////loading框
-//private var loadingDialog: MaterialDialog? = null
-//
-///**
-// * 打开等待框
-// */
-//fun AppCompatActivity.showLoadingDialog(message: String = "请求网络中") {
-//    if (!this.isFinishing) {
-//        if (loadingDialog == null) {
-//            loadingDialog = MaterialDialog(this)
-//                .cancelable(true)
-//                .cancelOnTouchOutside(false)
-//                .cornerRadius(12f)
-//                .customView(R.layout.layout_custom_progress_dialog_view)
-//                .lifecycleOwner(this)
-//            loadingDialog?.getCustomView()?.run {
-//                this.findViewById<androidx.appcompat.widget.AppCompatTextView>(R.id.loading_tips).text = message
-////                this.findViewById<ProgressBar>(R.id.progressBar).indeterminateTintList = SettingUtil.getOneColorStateList(this@showLoadingExt)
-//            }
-//        }
-//        loadingDialog?.show()
-//    }
-//}
-//
-///**
-// * 打开等待框
-// */
-//fun Fragment.showLoadingDialog(message: String = "请求网络中") {
-//    activity?.let {
-//        if (!it.isFinishing) {
-//            if (loadingDialog == null) {
-//                loadingDialog = MaterialDialog(it)
-//                    .cancelable(true)
-//                    .cancelOnTouchOutside(false)
-//                    .customView(R.layout.layout_custom_progress_dialog_view)
-//                    .lifecycleOwner(viewLifecycleOwner)
-//                loadingDialog?.getCustomView()?.run {
-//                    this.findViewById<androidx.appcompat.widget.AppCompatTextView>(R.id.loading_tips).text = message
-////                    this.findViewById<ProgressBar>(R.id.progressBar).indeterminateTintList = SettingUtil.getOneColorStateList(it)
-//                }
-//            }
-//            loadingDialog?.show()
-//        }
-//    }
-//}
-//
-///**
-// * 关闭等待框
-// */
-//fun Activity.dismissLoadingDialog() {
-//    loadingDialog?.dismiss()
-//    loadingDialog = null
-//}
-//
-///**
-// * 关闭等待框
-// */
-//fun Fragment.dismissLoadingDialog() {
-//    loadingDialog?.dismiss()
-//    loadingDialog = null
-//}
-/******************* loading框 版本1 end *************************/
 
-/******************* loading框 版本2 start TODO 此版本存在问题 *************************/
-///**
-// * 打开等待框
-// */
-//fun AppCompatActivity.showLoadingDialog(message: String = "请求网络中") {
-//    if (!this.isFinishing) {
-//        val loadingDialog = MaterialDialog(this)
-//            .cancelable(true)
-//            .cancelOnTouchOutside(false)
-//            .cornerRadius(12f)
-//            .customView(R.layout.layout_custom_progress_dialog_view)
-//            .lifecycleOwner(this)
-//
-//        loadingDialog.getCustomView().findViewById<androidx.appcompat.widget.AppCompatTextView>(R.id.loading_tips)?.text = message
-//
-//        loadingDialog.show()
-//    }
-//}
-//
-///**
-// * 关闭等待框
-// */
-//fun AppCompatActivity.dismissLoadingDialog() {
-//    try {
-//        val dialog = this::class.java.getDeclaredField("loadingDialog")
-//        dialog.isAccessible = true
-//        (dialog.get(this) as? MaterialDialog)?.dismiss()
-//        dialog.set(this, null)
-//    } catch (e: Exception) {
-//        e.printStackTrace()
-//    }
-//}
-//
-///**
-// * 在 Fragment 中显示等待框
-// */
-//fun Fragment.showLoadingDialog(message: String = "请求网络中") {
-//    if (!isAdded) return  // 确保 Fragment 已经添加到其宿主 Activity
-//
-//    (activity as? AppCompatActivity)?.showLoadingDialog(message)
-//}
-//
-///**
-// * 在 Fragment 中关闭等待框
-// */
-//fun Fragment.dismissLoadingDialog() {
-//    if (!isAdded) return  // 确保 Fragment 已经添加到其宿主 Activity
-//
-//    (activity as? AppCompatActivity)?.dismissLoadingDialog()
-//}
-/******************* loading框 版本2 end *************************/
-
-fun AppCompatActivity.showLoadingDialog(message: String = "请求网络中") {
+// 公共扩展函数：显示加载对话框
+fun FragmentActivity.showLoadingDialog(message: String = "请求网络中") {
     val fragmentManager = supportFragmentManager
-    val existingDialog = fragmentManager.findFragmentByTag("loadingDialog")
-    if (existingDialog == null) {
-        val dialogFragment = LoadingDialogFragment.newInstance(message)
-        dialogFragment.show(fragmentManager, "loadingDialog")
+    if (fragmentManager.findFragmentByTag(LoadingDialogFragment.TAG) == null) {
+        LoadingDialogFragment.newInstance(message).showNow(fragmentManager, LoadingDialogFragment.TAG)
     }
 }
 
-fun AppCompatActivity.dismissLoadingDialog() {
-    val fragmentManager = supportFragmentManager
-    val existingDialog = fragmentManager.findFragmentByTag("loadingDialog") as? DialogFragment
-    existingDialog?.dismiss()
+fun FragmentActivity.dismissLoadingDialog() {
+    supportFragmentManager.findFragmentByTag(LoadingDialogFragment.TAG)?.let {
+        (it as? DialogFragment)?.dismissAllowingStateLoss()
+    }
 }
 
-
+// 公共扩展函数：显示加载对话框（用于Fragment）
 fun Fragment.showLoadingDialog(message: String = "请求网络中") {
-    if (!isAdded) return  // 确保 Fragment 已经添加到其宿主 Activity
-
-    val fragmentManager = childFragmentManager
-    val existingDialog = fragmentManager.findFragmentByTag("loadingDialog")
-    if (existingDialog == null) {
-        val dialogFragment = LoadingDialogFragment.newInstance(message)
-        dialogFragment.show(fragmentManager, "loadingDialog")
+    if (isAdded && childFragmentManager.findFragmentByTag(LoadingDialogFragment.TAG) == null) {
+        LoadingDialogFragment.newInstance(message).showNow(childFragmentManager, LoadingDialogFragment.TAG)
     }
 }
 
 fun Fragment.dismissLoadingDialog() {
-    if (!isAdded) return  // 确保 Fragment 已经添加到其宿主 Activity
-
-    val fragmentManager = childFragmentManager
-    val existingDialog = fragmentManager.findFragmentByTag("loadingDialog") as? DialogFragment
-    existingDialog?.dismiss()
-}
-
-
-fun Activity.showWaitDialog(message: String = "请求网络中") {
-    WaitDialog.show(message).onBackPressedListener = OnBackPressedListener {
-        WaitDialog.dismiss()
-        return@OnBackPressedListener false
+    if (isAdded) {
+        childFragmentManager.findFragmentByTag(LoadingDialogFragment.TAG)?.let {
+            (it as? DialogFragment)?.dismissAllowingStateLoss()
+        }
     }
 }
 
-fun Activity.dismissWaitDialog() {
-    WaitDialog.dismiss()
-}
 
-fun Fragment.showWaitDialog(message: String = "请求网络中") {
-    WaitDialog.show(message).onBackPressedListener = OnBackPressedListener {
-        WaitDialog.dismiss()
-        return@OnBackPressedListener false
-    }
-}
-
-fun Fragment.dismissWaitDialog() {
-    WaitDialog.dismiss()
-}
