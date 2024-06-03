f24a0916b37cc9dd80a6d47b64e228814cf27be3
Lumberjack <gongheoffice@163.com>
2024-06-03 20:14:27 +0800

    fea_v5.0: 1.修复 bug

--- app/src/main/java/com/shmedo/mcloudapp/device/viewmodel/state/M20SWorkModelViewModel.kt
+++ app/src/main/java/com/shmedo/mcloudapp/device/viewmodel/state/M20SWorkModelViewModel.kt
@@ -5,6 +5,7 @@ import com.shmedo.lib.core.base.viewmodel.NonNullObservableField
 
 class M20SWorkModelViewModel : ViewModel() {
     val isEditable = NonNullObservableField(true)
+    val isFrontCalcVisible = NonNullObservableField(true)
 
     val model = NonNullObservableField("")//模式
     val frontCalc = NonNullObservableField("")//0:关闭前端解算  1:打开前端解算  2:根据网络状态开启前端解算  默认2
