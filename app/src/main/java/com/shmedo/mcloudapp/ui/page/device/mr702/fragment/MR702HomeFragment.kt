9bc76a623e975948bb66b124ff389db397d07a4e
宫贺 <he.gong@shmedo.cn>
2024-08-28 13:15:24 +0800

    fea_v5.0: MR702设备蓝牙连接添加心跳包维持连接

--- app/src/main/java/com/shmedo/mcloudapp/ui/page/device/mr702/fragment/MR702HomeFragment.kt
+++ app/src/main/java/com/shmedo/mcloudapp/ui/page/device/mr702/fragment/MR702HomeFragment.kt
@@ -3,11 +3,13 @@ package com.shmedo.mcloudapp.ui.page.device.mr702.fragment
 import android.os.Bundle
 import com.blankj.utilcode.util.ConvertUtils
 import com.blankj.utilcode.util.StringUtils
+import com.blankj.utilcode.util.TimeUtils
 import com.drake.brv.utils.models
 import com.drake.brv.utils.setup
 import com.google.android.flexbox.FlexboxLayoutManager
 import com.hjq.toast.Toaster
 import com.kunminx.architecture.ui.page.DataBindingConfig
+import com.shmedo.core.commonlib.utils.AppContants
 import com.shmedo.lib.cmd.base.iot_cmd.assemble.entity.common.WorkModeEntity
 import com.shmedo.lib.cmd.base.iot_cmd.enums.IOTCommandType
 import com.shmedo.lib.cmd.base.iot_cmd.model.common.CommonSettingCmdResult
@@ -44,6 +46,7 @@ import com.shmedo.mcloudapp.ui.page.device.common.QueryDeviceDataFragment
 import com.shmedo.mcloudapp.ui.viewmodel.state.MR702HomeViewModel
 import com.shmedo.mcloudapp.ui.viewmodel.state.ToolbarViewModel
 import com.shmedo.mcloudapp.ui.widget.recyclerview.MyGridSpacingItemDecoration
+import kotlinx.coroutines.flow.debounce
 import org.koin.android.ext.android.inject
 import timber.log.Timber
 
@@ -310,6 +313,7 @@ class MR702HomeFragment : BaseIOTDeviceFragment() {
     }
 
     override fun setResultData(cmdStr: String) {
+        updateLastCommunicationTime()
         when (IOTCommandUtil.extractCommandType(cmdStr)) {
             IOTCommandType.GET_WORK_MODE -> {
                 val result =
@@ -449,4 +453,29 @@ class MR702HomeFragment : BaseIOTDeviceFragment() {
         }
         binding.rvModule.models = moduleList
     }
+
+    override fun createObserver() {
+        super.createObserver()
+        setupHeartbeat()
+    }
+
+    // 设置心跳检查
+    private fun setupHeartbeat() {
+        launchWithViewLifecycle {
+            lastCommunicationTime
+                .debounce(AppContants.Communication.DELAY_10000_MILLIS)  // 30秒无更新触发
+                .collect { lastUpdateTime ->
+                    val updateTime = TimeUtils.millis2String(lastUpdateTime, "yyyy-MM-dd HH:mm:ss")
+                    Timber.d("startTime: ${TimeUtils.getNowString()}，lastUpdateTime：$updateTime")
+                    // 仅当设备连接并且需要发送心跳时，才发送心跳包
+                    if (mHeadStates.isConnected.get() && isNearbyCommunicationTimeout(lastUpdateTime)) {
+                        Timber.d("bingo startTime: ${TimeUtils.getNowString()}，lastUpdateTime：$updateTime")
+                        val command = IOTCommandUtil.getCommand(IOTCommandType.HEART_BEAT)
+                        Timber.d("发送心跳包指令: $command")
+                        sendBleCommand(command)
+                    }
+                }
+        }
+    }
+
 }
