00523ee8f27961d6f34af52814c48485413370da
宫贺 <he.gong@shmedo.cn>
2025-05-20 14:22:02 +0800

    优化M20S设备状态管理逻辑，新增设备故障检测，确保设备状态与自检结果一致，完善状态信息展示。

--- app/src/main/java/com/shmedo/mcloudapp/ui/page/device/m20s/fragment/M20SBaseInfoFragment.kt
+++ app/src/main/java/com/shmedo/mcloudapp/ui/page/device/m20s/fragment/M20SBaseInfoFragment.kt
@@ -2,18 +2,21 @@ package com.shmedo.mcloudapp.ui.page.device.m20s.fragment
 
 import android.os.Bundle
 import android.util.Log
+import com.blankj.utilcode.util.ColorUtils
 import com.blankj.utilcode.util.ConvertUtils
 import com.drake.brv.utils.models
 import com.shmedo.core.commonlib.jsonhelper.MoshiUtil
 import com.shmedo.core.commonlib.utils.AppContants
 import com.shmedo.lib.cmd.base.iot_cmd.model.common.CommonCurrentStateInfo
 import com.shmedo.lib.network.ext.errorMsg
+import com.shmedo.mcloudapp.R
 import com.shmedo.mcloudapp.extensions.launchWithViewLifecycle
 import com.shmedo.mcloudapp.extensions.notNullKey
 import com.shmedo.mcloudapp.model.DeviceStatusInfoBasicItem
 import com.shmedo.mcloudapp.model.DeviceStatusInfoGroupItem
 import com.shmedo.mcloudapp.model.GapItem
 import com.shmedo.mcloudapp.ui.page.device.common.BaseDeviceStatusInfoStyle2Fragment
+import com.shmedo.mcloudapp.utils.DeviceStatusHelper
 import com.shmedo.mcloudapp.utils.DeviceStatusInfoProcessor
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -50,25 +53,27 @@ class M20SBaseInfoFragment : BaseDeviceStatusInfoStyle2Fragment() {
                     name = "设备SN",
                     value = stateInfo.sn,
                 )
-//                val deviceStatus = when (stateInfo.deviceStatus) {
-//                    "-2" -> "告警"
-//                    "-3" -> "故障"
-//                    else -> "正常"
-//                }
-//                groupList.add(
-//                    DeviceStatusInfoBasicItem(
-//                        name = "设备状态",
-//                        value = deviceStatus,
-//                        textColorRes = when (deviceStatus) {
-//                            "正常" -> ColorUtils.getColor(R.color.online_colorPrimary)
-//                            "告警" -> ColorUtils.getColor(
-//                                R.color.warn_FF9D00
-//                            )
-//
-//                            else -> ColorUtils.getColor(R.color.error_FF4400)
-//                        }
-//                    )
-//                )
+
+                val deviceAbnormalList =
+                    if (stateInfo.self_check.isEmpty()) arrayListOf<String>() else DeviceStatusHelper.checkDeviceAbnormal(
+                        stateInfo.self_check
+                    )
+                val deviceStatus = if (deviceAbnormalList.isEmpty()) "正常" else "故障"
+
+                groupList.add(
+                    DeviceStatusInfoBasicItem(
+                        name = "设备状态",
+                        value = deviceStatus,
+                        textColorRes = when (deviceStatus) {
+                            "正常" -> ColorUtils.getColor(R.color.online_colorPrimary)
+                            "告警" -> ColorUtils.getColor(
+                                R.color.warn_FF9D00
+                            )
+
+                            else -> ColorUtils.getColor(R.color.error_FF4400)
+                        }
+                    )
+                )
                 DeviceStatusInfoProcessor.addDeviceStatusInfoBasicItemFromString(
                     groupList,
                     name = "硬件版本",
