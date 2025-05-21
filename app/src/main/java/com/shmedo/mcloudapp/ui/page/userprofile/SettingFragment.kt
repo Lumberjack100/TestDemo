4a9c40380684449bbf60125a86fd021a2210a414
宫贺 <he.gong@shmedo.cn>
2025-05-21 15:34:09 +0800

    优化设置界面，重构ViewModel使用方式，新增消息通知权限检查功能，更新数据绑定逻辑，改善UI布局，提升用户体验。

--- app/src/main/java/com/shmedo/mcloudapp/ui/page/userprofile/SettingFragment.kt
+++ app/src/main/java/com/shmedo/mcloudapp/ui/page/userprofile/SettingFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.viewModels
 import com.blankj.utilcode.util.CleanUtils
+import com.hjq.permissions.Permission
+import com.hjq.permissions.XXPermissions
 import com.hjq.toast.Toaster
 import com.kunminx.architecture.ui.page.DataBindingConfig
 import com.shmedo.mcloudapp.BR
@@ -11,7 +13,6 @@ import com.shmedo.mcloudapp.R
 import com.shmedo.mcloudapp.baseclickproxy.BaseClickProxy
 import com.shmedo.mcloudapp.databinding.FragmentSettingBinding
 import com.shmedo.mcloudapp.extensions.getAppViewModel
-import com.shmedo.mcloudapp.extensions.getFragmentScopeViewModel
 import com.shmedo.mcloudapp.extensions.launchWithViewLifecycle
 import com.shmedo.mcloudapp.extensions.nav
 import com.shmedo.mcloudapp.extensions.safeNavigate
@@ -23,24 +24,22 @@ import com.shmedo.mcloudapp.ui.viewmodel.state.SettingViewModel
 import com.shmedo.mcloudapp.ui.viewmodel.state.ToolbarViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import org.koin.androidx.viewmodel.ext.android.getViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SettingFragment : BaseFragment() {
     private lateinit var binding: FragmentSettingBinding
     private lateinit var mMessenger: PageMessenger
     private val toolbarViewModel: ToolbarViewModel by viewModels()
-    private lateinit var mStates: SettingViewModel
-    private lateinit var logViewModel: LogViewModel
+    private val mStates: SettingViewModel by viewModels()
+    private val logViewModel: LogViewModel by viewModel()
 
 
     override fun initViewModel() {
         mMessenger = getAppViewModel()
-        mStates = getFragmentScopeViewModel()
-        logViewModel = getViewModel()
     }
 
     override fun getDataBindingConfig(): DataBindingConfig {
-        return DataBindingConfig(R.layout.fragment_setting, BR.vm, mStates)
+        return DataBindingConfig(R.layout.fragment_setting, BR.stateVM, mStates)
             .addBindingParam(BR.toolbarVM, toolbarViewModel)
             .addBindingParam(BR.click, ClickProxy())
     }
@@ -67,7 +66,48 @@ class SettingFragment : BaseFragment() {
 
     }
 
+    override fun onResume() {
+        super.onResume()
+        initImmersionBar(binding.llToolbar.toolbar)
+        checkNotificationPermission()
+    }
+
+    private fun checkNotificationPermission() {
+        mStates.isAllowNotification.set(XXPermissions.isGranted(requireContext(), Permission.POST_NOTIFICATIONS))
+    }
+
     inner class ClickProxy : BaseClickProxy() {
+        /**
+         * 消息通知
+         */
+        fun onMessageNotificationClick() {
+            XXPermissions.startPermissionActivity(
+                this@SettingFragment,
+                Permission.POST_NOTIFICATIONS
+            )
+        }
+
+        /**
+         * 权限设置
+         */
+        fun onPermissionSettingClick() {
+
+        }
+
+        /**
+         * 个人信息收集清单
+         */
+        fun onPersonalInformationCollectionListClick() {
+
+        }
+
+        /**
+         * 第三方信息共享清单
+         */
+        fun onThirdPartyInformationSharingListClick() {
+
+        }
+
         /**
          * 清除缓存和历史日志
          */
@@ -85,28 +125,6 @@ class SettingFragment : BaseFragment() {
         }
 
         /**
-         * 服务协议
-         */
-        fun onUserProtocolClick() {
-            val url = "file:///android_asset/private/UserProtocol.html"
-            WebviewActivity.startActivity(mActivity, "用户协议与免责条款", url)
-        }
-
-        /**
-         * 隐私政策
-         */
-        fun onPrivacyPolicyClick() {
-
-        }
-
-        /**
-         * 帮助与反馈
-         */
-        fun onHelpAndFeedbackClick() {
-//            nav().safeNavigate(R.id.action_mainFragment_to_helpAndFeedbackFragment)
-        }
-
-        /**
          * 关于
          */
         fun onAboutUsClick() {
@@ -116,14 +134,18 @@ class SettingFragment : BaseFragment() {
         /**
          * 应用日志查看
          */
-        fun onViewLogClick() {
+        fun onLogViewClick() {
             nav().safeNavigate(R.id.action_global_to_logSessionListFragment)
         }
 
+        /**
+         * 服务协议
+         */
+        fun onUserProtocolClick() {
+            val url = "file:///android_asset/private/UserProtocol.html"
+            WebviewActivity.startActivity(mActivity, "用户协议与免责条款", url)
+        }
     }
 
-    override fun onResume() {
-        super.onResume()
-        initImmersionBar(binding.llToolbar.toolbar)
-    }
+
 }
--- app/src/main/java/com/shmedo/mcloudapp/ui/viewmodel/state/SettingViewModel.kt
+++ app/src/main/java/com/shmedo/mcloudapp/ui/viewmodel/state/SettingViewModel.kt
@@ -1,7 +1,8 @@
 package com.shmedo.mcloudapp.ui.viewmodel.state
 
 import androidx.lifecycle.ViewModel
+import com.shmedo.mcloudapp.ui.page.base.viewmodel.NonNullObservableField
 
 class SettingViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val isAllowNotification = NonNullObservableField(false) //是否
 }
