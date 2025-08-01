8a56ed4242e07f714e44b3a50ad6af2ceb166add
宫贺 <he.gong@shmedo.cn>
2025-08-01 15:16:31 +0800

    更新版本号至 5.3.1 和 5.3.5，新增一体式自供电 GNSS 接收机(M50)和普适型 GNSS 接收机(M20S)的配置优化，以及远程调试功能的优化。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,11 +11,11 @@ plugins {
 
 val majorVersion = 5
 val minorVersion = 3
-val patchVersion = 0
+val patchVersion = 1
 
 val testMajorVersion = 5
 val testMinorVersion = 3
-val testPatchVersion = 4
+val testPatchVersion = 5
 
 /**
  * 获取Git库HEAD的SHA1码前5位
