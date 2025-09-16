0de7aedc50d280895a304720dff0715089e9c1fe
宫贺 <he.gong@shmedo.cn>
2025-09-16 14:43:56 +0800

    更新版本至5.3.19、5.3.7，物联网采集器(DAS)增加遥测功能。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,11 +11,11 @@ plugins {
 
 val majorVersion = 5
 val minorVersion = 3
-val patchVersion = 6
+val patchVersion = 7
 
 val testMajorVersion = 5
 val testMinorVersion = 3
-val testPatchVersion = 18
+val testPatchVersion = 19
 
 /**
  * 获取Git库HEAD的SHA1码前5位
